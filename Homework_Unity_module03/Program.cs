b69efee91c648e1c061e2a10e4304699b140c2a2
Rainbow Monkey <de43gy@gmail.com>
2020-04-17 16:07:20 +0300

    Add project files.

---
+++ Homework_Unity_module03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Unity_module03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
